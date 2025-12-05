d4f68076c5a98704de41a489b0d1de44e7cda1e1
Thoth-source <readerappdev@proton.me>
2025-12-05 14:56:18 +0200

    Initial commit: Reader app

--- push-to-github.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0push-to-github.ps1"
-pause
--- setup-github.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0setup-github.ps1"
-pause
